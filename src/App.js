c2f3c088e4f5164cc684b0b981d4fec6a2d8b2ae
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-10 22:32:57 -0400

    Added Route To Update Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home.js';
+import Updates from './routes/Updates.js';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/updates' element={<Updates />} />
       </Routes>
     </>
   );
