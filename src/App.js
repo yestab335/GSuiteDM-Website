c2d25a3d70651b18149be453b51beb4df298ca8d
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-10 23:32:38 -0400

    Added Route For 404 Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home.js';
 import Updates from './routes/Updates.js';
+import Error from './routes/PageNotFound.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/updates' element={<Updates />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </>
   );
