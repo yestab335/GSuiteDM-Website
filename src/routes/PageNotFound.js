40d28ff84486f69ca143a2c0b2c18e157e055370
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-15 01:07:12 -0400

    Refactored Code

--- src/routes/PageNotFound.js
+++ src/routes/PageNotFound.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 // Lottie For Animation Gif
 import Lottie from "lottie-react";
 import errorAnimation from "../assets/errorAnimation.json";
