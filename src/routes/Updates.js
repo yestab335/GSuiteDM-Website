e8cc37e2a6f3bd65d171c3a13ec8900a0f83c3ee
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-21 17:20:50 -0400

    Added Update Log Component

--- src/routes/Updates.js
+++ src/routes/Updates.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/navigation/Navbar.jsx';
+import UpdateLog from '../components/UpdateLog.jsx';
 import Footer from '../components/navigation/Footer.jsx';
 
 const Updates = () => {
   return (
     <>
       <Navbar />
+      <UpdateLog />
       <Footer />
     </>
   );
