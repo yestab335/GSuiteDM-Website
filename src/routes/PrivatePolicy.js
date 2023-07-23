c57a0370c27a01a2e05b44d8abb9758c932f391f
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-23 00:37:43 -0400

    Private Policy Page Component Compiler

---
+++ src/routes/PrivatePolicy.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/navigation/Navbar.jsx';
+import Page from '../components/PrivatePolicy.jsx';
+import Footer from '../components/navigation/Footer.jsx';
+
+const PrivatePolicy = () => {
+  return (
+    <>
+      <Navbar />
+      <Page />
+      <Footer />
+    </>
+  );
+}
+
+export default PrivatePolicy;
