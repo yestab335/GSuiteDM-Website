08d22071fe6a675c72081e10155441bf14b6e697
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-07-23 00:36:26 -0400

    Added Route Path To Priavcy Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home.js';
 import Updates from './routes/Updates.js';
+import Privacy from './routes/PrivatePolicy.js';
 import Error from './routes/PageNotFound.js';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/updates' element={<Updates />} />
+        <Route path='/privacy' element={<Privacy />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </>
