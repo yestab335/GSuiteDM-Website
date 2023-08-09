10249acd717efc738c9f8c32f72253d443436aab
yestab335 <yestab335@gmail.com>
2023-08-09 18:16:19 -0400

    Added Navigation Bar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/navigation/Navbar"
 import Footer from "@/components/navigation/Footer"
 
 export default function Home() {
   return (
     <main className="">
+      <Navbar />
       <Footer />
     </main>
   )
