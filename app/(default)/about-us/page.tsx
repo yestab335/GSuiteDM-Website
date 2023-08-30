973c97619eabc4b9f68b1b93307f1e55159bc27c
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-08-29 21:57:18 -0400

    Initial Commit

---
+++ app/(default)/about-us/page.tsx
@@ -0,0 +1,25 @@
+export const metadata = {
+  title: 'About Us - GSuiteDM',
+  description: 'Created by Yasser Tabora',
+}
+
+export default function About() {
+  return (
+    <>
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          {/* About Us Content */}
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            {/* Section Header */}
+            <div className="text-center pb-12 md:pb-16">
+              <h1 className="text-5xl md:text6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">About Us</h1>
+              <div className="max-w-3xl mx-auto">
+                <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">I am an independent person</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
