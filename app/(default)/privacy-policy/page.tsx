29b6efd6889a0f31b2c65c4aa5dca9f2c1e61d52
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-09-04 21:58:52 -0400

    Initial Commit

---
+++ app/(default)/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+import PrivacyPolicy from '@/components/PrivacyPolicy'
+
+export const metadata = {
+  title: 'Privacy Policy - GSuiteDM',
+  description: 'Created by Yasser Tabora',
+}
+
+export default function Privacy() {
+  return (
+    <>
+      <PrivacyPolicy />
+    </>
+  )
+}
