58dc3523dd402f58a320aecf3b2f040b6a6069b5
Yasser Madouch <yestab335@gmail.com>
2024-05-17 23:36:52 -0400

    Added dynamic dark mode

--- app/(default)/docs/page.tsx
+++ app/(default)/docs/page.tsx
@@ -8,14 +8,14 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <section className="relative">
+      <section className="relative dark:bg-black-600">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           {/* About Us Content */}
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
             {/* Section Header */}
             <div className="text-center pb-12 md:pb-16">
               <h1
-                className="text-5xl md:text6xl font-extrabold leading-tighter tracking-tighter mb-4"
+                className="text-5xl md:text6xl font-extrabold leading-tighter tracking-tighter mb-4 dark:text-white"
                 data-aos="zoom-y-out"
               >
                 Coming Soon
