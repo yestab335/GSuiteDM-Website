2b64f780a5064be5c911d6cd32f5fde48228ba74
Yasser Madouch <yestab335@gmail.com>
2024-05-18 21:22:24 -0400

    Removed unused imports and added dynamic dark mode

--- app/(default)/gsuite/page.tsx
+++ app/(default)/gsuite/page.tsx
@@ -1,5 +1,3 @@
-// Imports
-
 export const metadata = {
   title: 'Add GSuiteDM - GSuiteDM',
   description: 'Created by Yasser Tabora',
@@ -8,14 +6,14 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <section className="relative">
+      <section className="relative dark:bg-black-600 text-white">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
-          {/* About Us Content */}
+          {/* Page content */}
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            {/* Section Header */}
+            {/* Section header */}
             <div className="text-center pb-12 md:pb-16">
               <h1
-                className="text-5xl md:text6xl font-extrabold leading-tighter tracking-tighter mb-4"
+                className="text-5xl md:text6xl font-extrabold leading-tighter tracking-tighter mb-4 dark:text-white"
                 data-aos="zoom-y-out"
               >
                 Coming Soon
